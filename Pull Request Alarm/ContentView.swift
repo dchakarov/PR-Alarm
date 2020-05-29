c0206cbda286b7a2cad81cfc8f468ec520b8217b
Dimitar Chakarov <dimitar@dchakarov.com>
2020-05-29 23:55:57 +0100

    Initial Commit

---
+++ Pull Request Alarm/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Pull Request Alarm
+//
+//  Created by Dimitar Chakarov on 29/05/2020.
+//  Copyright © 2020 Dimitar Chakarov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
